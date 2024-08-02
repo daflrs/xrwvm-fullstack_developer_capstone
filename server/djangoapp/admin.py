20f4a943557290ad430af33ab153918156bdf7d7
daflrs <dangienew2010s@gmail.com>
2024-08-02 04:21:11 -0400

    Add car makes and models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,5 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
@@ -7,7 +7,7 @@
 # CarModelInline class
 
 # CarModelAdmin class
-
+admin.site.register(CarMake)
 # CarMakeAdmin class with CarModelInline
-
+admin.site.register(CarModel)
 # Register models here
